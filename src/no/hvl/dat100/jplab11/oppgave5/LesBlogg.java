88fd21552b0bf5fa7f43a93c94e2c9ac574b3ea7
Krismaster <69586032+594623@users.noreply.github.com>
2020-11-10 10:24:41 +0100

    Ferdig oppgave 5

--- src/no/hvl/dat100/jplab11/oppgave5/LesBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave5/LesBlogg.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.util.Scanner;
 
 import no.hvl.dat100.jplab11.common.TODO;
@@ -21,8 +22,39 @@ public class LesBlogg {
 	private static String BILDE = "BILDE";
 
 	public static Blogg les(String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
-
+		
+		try {
+			Blogg blogg = new Blogg();
+			BufferedReader br = new BufferedReader(new FileReader(mappe + filnavn));
+			String l = br.readLine();
+			while (l != null) {
+				if (l.equals(TEKST)) {
+					Tekst t = new Tekst(
+							Integer.parseInt(br.readLine()),
+							br.readLine(),
+							br.readLine(),
+							Integer.parseInt(br.readLine()),
+							br.readLine()
+					);
+					blogg.leggTil(t);
+				} else if (l.equals(BILDE)) {
+					Bilde b = new Bilde(
+							Integer.parseInt(br.readLine()),
+							br.readLine(),
+							br.readLine(),
+							Integer.parseInt(br.readLine()),
+							br.readLine(),
+							br.readLine()
+					);
+					blogg.leggTil(b);
+				}
+				l = br.readLine();
+			}
+			br.close();
+			return blogg;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
